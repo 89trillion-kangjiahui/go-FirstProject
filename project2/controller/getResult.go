4896817e31aa28b97edf1c962fec1ccd6d10802d
kangjiahui <2241352990@qq.com>
2021-06-29 15:35:36 +0800

    修改了题目2controller层的代码逻辑

--- project2/controller/getResult.go
+++ project2/controller/getResult.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"project2/util"
 	"strings"
 	"unicode"
 
@@ -8,7 +9,6 @@ import (
 	"net/http"
 
 	"project2/dto"
-	"project2/util"
 )
 
 func ComputeRet(c *gin.Context) {
@@ -19,12 +19,10 @@ func ComputeRet(c *gin.Context) {
 	if exp == "" {
 		ret := dto.SetResult(4001, 0, "你输入的表达式为空")
 		c.JSON(http.StatusOK, ret)
-	}
-	if !StringTrue(exp) {
+	}else if !StringTrue(exp) {
 		ret := dto.SetResult(4002, 0, "你输入的表达式算数表达式不正确")
 		c.JSON(http.StatusOK, ret)
-	}
-	if exp != "" {
+	}else {
 		solution := util.GetResult(util.MixToPost(exp))
 		//封装结果
 		Ret := dto.SetResult(200, solution, "计算成功")
@@ -34,21 +32,25 @@ func ComputeRet(c *gin.Context) {
 
 // 判断算数表达式是否正确
 func StringTrue(exp string) bool {
+	//如果算数表达式的第一个位置的元素不是数字或者最后一个位置的元素不是数字返回false
+	if!unicode.IsDigit(rune(exp[0])) || !unicode.IsDigit(rune(exp[len(exp)-1])) {
+		return false
+	}
 	flag := false
 	for _, char := range exp {
 		//如果字符为空，跳过判断
 		if char == ' ' {
 			continue
 		} else if !unicode.IsDigit(rune(char)) && flag == false {
+			//如果当前不是数字并且还没有开启运算符检查，则将开启下一个是否为运算符的检测
 			flag = true
 		} else if !unicode.IsDigit(rune(char)) && flag == true {
+			//如果当前不是数字并且开启了运算符检测，则返回false
 			return false
 		} else if unicode.IsDigit(rune(char)) && flag == true {
+			//如果当前是数字并且开启了运算符检测，则关闭运算符检测
 			flag = false
 		}
-	}
-	if !unicode.IsDigit(rune(exp[len(exp)-1])) {
-		return false
 	}
 	return true
 }
